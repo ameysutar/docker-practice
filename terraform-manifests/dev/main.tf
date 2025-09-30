699af3462f29767432574b70a898e4abe0675cad
ameysutar <70933709+ameysutar@users.noreply.github.com>
2025-09-30 10:31:19 +0530

    Update main.tf

--- terraform-manifests/dev/main.tf
+++ terraform-manifests/dev/main.tf
@@ -15,13 +15,14 @@ module "infra_services" {
   cloud_env = "dev"
   vpc_tag_name = "dev_vpc"
   instance_count = "2"
-  instance_type = "t2.micro"
+  instance_type = "t3.micro"
   vpc_cidr = "172.31.0.0/16"
   public_cidrs = ["172.31.3.0/24","172.31.4.0/24"]
   private_cidrs = ["172.31.5.0/24","172.31.6.0/24"]
   public_cidr = "172.31.1.0/24"
   private_cidr = "172.31.2.0/24"
-  bucket_name = "testing-terraform-s3-bucket-data"
+  bucket_name = "testing-terraform-s3-bucket-data-282025"
   bucket_acl  = "private"
-  instance_key_name = "test-tf-key"
+  instance_key_name = "key1"
+
 }
